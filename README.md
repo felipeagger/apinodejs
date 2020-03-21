2a01b3ae9948db34156eae14d5f6745abe54dd15
Felipe <felipe@localhost.localdomain>
2020-03-21 14:08:36 -0300

    Added deployment for k8s

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 Exemplo API em NodeJS
 
 Instalar dependencias: npm install -y
+
+docker pull felipeagger/hello-world-nodejs:latest
+
+Run on K8s:
+
+kubectl create -f nodejs-deployment.yaml
--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 var express = require('express');
+var os = require("os");
 var app = express();
-var porta = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var porta = process.env.PORT || 8080;
+var hostname = os.hostname();
 
 app.get('/', function (req, res) {
-  res.send('Hello World - by NodeJS!');
+  res.send('Hello World - by NodeJS!\nHostname: '+hostname);
 });
 
 app.listen(porta, function () {
